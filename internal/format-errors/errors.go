34ac7cea9b486bf968907f640c0db55053b7a6d6
Susilnem <susilsaurav@gmail.com>
2023-09-24 22:30:23 +0545

    :hammer: Add require Errors Fun

---
+++ internal/format-errors/errors.go
@@ -0,0 +1,29 @@
+package errors
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func RecordNotFound(c *gin.Context, err error, errMessage ...string) {
+	errorMessage := "Record not found"
+	if len(errMessage) > 0 {
+		errorMessage = errMessage[0]
+	}
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, gin.H{"message": errorMessage})
+		return
+	}
+
+	InternalServerError(c)
+
+}
+
+func InternalServerError(c *gin.Context) {
+	c.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
+	return
+}
